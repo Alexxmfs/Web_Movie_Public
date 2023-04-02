60e3cf18f46cb2539ae72104dd2230bd90683e6a
Alexxmfs <alexxmfs@hotmail.com>
2023-04-02 16:06:53 -0300

    successful✔

---
+++ client/src/components/common/ProtectedPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Children } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setAuthModalOpen } from "../../redux/features/authModalSlice";
+
+const ProtectedPage = ({ children }) => {
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(setAuthModalOpen(!user));
+}, [user, dispatch]);
+  
+
+  return (
+    user ? children : null
+    );
+};
+
+export default ProtectedPage;
---
+++ server/src/models/model.options.js
@@ -0,0 +1,21 @@
+const modelOptions = {
+    toJSON: {
+        virtuals: true,
+        tranform: (_, obj) => {
+            delete obj._id;
+            return obj
+        }
+    },
+    
+    toObject: {
+        virtuals: true,
+        tranform: (_, obj) => {
+            delete obj._id;
+            return obj
+        }
+    },
+    versionKey: false,
+    timestamps: true
+}
+
+export default modelOptions;
